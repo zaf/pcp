fbc43669223b72b471413e00f9349d8db7a60288
Lefteris Zafiris <zaf@fastmail.com>
2023-03-04 15:20:40 +0200

    Use copy_file_range(2)

--- pcp.go
+++ pcp.go
@@ -21,7 +21,6 @@ import (
 	"log"
 	"os"
 	"runtime"
-	"runtime/debug"
 	"strings"
 	"sync"
 
@@ -30,7 +29,6 @@ import (
 
 var (
 	force   = flag.Bool("f", false, "Overwrite destination file if it exists.")
-	fsync   = flag.Bool("s", false, "Sync file to disk after done copying data.")
 	threads = flag.Int("t", 0, "Specifies the number of threads used to copy data simultaneously.")
 )
 
@@ -121,7 +119,7 @@ func pcopy(source, destination string) error {
 			endOffset = srcSize
 		}
 		wg.Add(1)
-		go mcopy(src, dst, startOffset, endOffset, wg)
+		go fcopy(src, dst, startOffset, endOffset, wg)
 		startOffset += chunk
 		endOffset += chunk
 	}
@@ -129,45 +127,30 @@ func pcopy(source, destination string) error {
 	return dst.Close()
 }
 
-// Map file chunks in memory and copy data
-func mcopy(src, dst *os.File, start, end int64, wg *sync.WaitGroup) {
+// Copy data in chunks using copy_file_range()
+func fcopy(src, dst *os.File, start, end int64, wg *sync.WaitGroup) {
 	defer wg.Done()
-	// Set runtime to panic instead of crashing on bus errors.
-	debug.SetPanicOnFault(true)
-	defer func() {
-		if e := recover(); e != nil {
-			log.Fatalln(e)
+	var n, written int
+	var err error
+	len := end - start
+	remain := len
+	for remain > 0 {
+		for {
+			n, err = unix.CopyFileRange(int(src.Fd()), &start, int(dst.Fd()), &start, int(end-start), 0)
+			if err != unix.EINTR {
+				break
+			}
 		}
-	}()
-	s, err := unix.Mmap(int(src.Fd()), start, int(end-start), unix.PROT_READ, unix.MAP_SHARED)
+		written += n
+		remain -= int64(n)
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer unix.Munmap(s)
-	err = unix.Madvise(s, unix.MADV_SEQUENTIAL)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	d, err := unix.Mmap(int(dst.Fd()), start, int(end-start), unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	n := copy(d, s)
-	if int64(n) != (end - start) {
-		unix.Munmap(d)
+	if int64(written) != len {
 		log.Fatalln("Short write")
 	}
-	if *fsync {
-		err = unix.Msync(d, unix.MS_SYNC)
-		if err != nil {
-			unix.Munmap(d)
-			log.Fatalln(err)
-		}
-	}
-	err = unix.Munmap(d)
-	if err != nil {
-		log.Fatalln(err)
-	}
+
 }
 
 // Align to OS page boundaries
